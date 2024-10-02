9517548334d22b76a0e71eef934d8853fb0695ec
LeChef318 <jan.hoffmann318@gmail.com>
2024-10-02 07:14:00 +0200

    node_web server

---
+++ server/node_webserver/helpers.js
@@ -0,0 +1,149 @@
+"use strict"
+import { readFile } from "fs/promises";
+import { normalize, resolve } from "path";
+const forbiddenReq = ["HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"];
+
+class Request {
+    _httpMethod
+    _resource
+    _protocol
+    _headers
+    _badRequest
+    constructor(buffer) {
+        this._badRequest = 0
+        this.parseRequest(buffer.toString());
+        this.checkHttpMethod();
+    }
+    parseRequest(bString) {
+        let request = bString.split("\r\n\r\n")
+        if(request.length != 2){
+            this._badRequest = 400
+            return
+        }
+        let headerArr = request[0].split("\r\n");
+        let reqHead = headerArr[0].split(" ");
+        if(reqHead.length != 3) {
+            this._badRequest = 400
+            return
+        }
+        this._httpMethod = reqHead[0]
+        this._resource = reqHead[1]
+        this._protocol = reqHead[2]
+        headerArr.shift()
+        this._headers = new Map()
+        for (let line of headerArr) {
+            let split = line.split(" ")
+            this._headers.set(split[0], split[1])
+        }
+        return
+    }
+    checkHttpMethod() {
+        if(forbiddenReq.includes(this._httpMethod)) {
+            this._badRequest = 405
+        }
+        return
+    }
+    get badRequest() {
+        return this._badRequest
+    }
+
+}
+
+class Response {
+    _protocol
+    _status
+    _statusMsg
+    _headers
+    _body
+
+    constructor() {
+        this._headers = new Map()
+    }
+    set protocol(newProtocol) {
+        this._protocol = newProtocol
+    }
+    set status(newStatus) {
+        this._status = newStatus
+    }
+    set statusMsg(newStatusMsg) {
+        this._statusMsg = newStatusMsg
+    }
+    set body(newBody) {
+        this._body = newBody
+    }
+    addHeader(fieldname, token) {
+        this._headers.set(fieldname, token)
+    }
+    async create4xx(errNr) {
+        this._protocol = "HTTP/1.1"
+        this._status = errNr
+        if(errNr == 400) {
+            this._statusMsg = "Bad request"
+            this.addHeader("Content-Type:", "application/json")
+            try {
+                this._body = await readFile("./resources/400.json", "utf-8")
+                let cntLength = this._body.length
+                this.addHeader("Content-Length:", cntLength.toString())
+            }
+            catch {
+                console.log("something went wrong")
+            }
+        }
+        else if(errNr == 404) {
+            this._statusMsg = "Not found"
+            this.addHeader("Content-Type:", "text/html; charset=UTF-8")
+            try {
+                this._body = await readFile("./resources/404.html", "utf-8")
+                let cntLength = this._body.length
+                this.addHeader("Content-Length:", cntLength.toString())
+            }
+            catch {
+                console.log("something went wrong")
+            }
+        }
+        else if(errNr == 405) {
+            this._statusMsg = "Method Not Allowed"
+            this.addHeader("Allow:", "GET")
+            this.addHeader("Content-Length:", "0")
+        }
+        else if(errNr == 403) {
+            this._statusMsg = 'Forbidden';
+            this.addHeader('Content-Type:', 'text/plain');
+            this._body = 'Access Denied';
+            this.addHeader('Content-Length:', this._body.length.toString());
+        }
+    }
+    async createResponse(resource) {
+        const rootDir = path.resolve('./resources')
+        let filepath = resource === '/' ? '/index.html' : resource
+        const normalizedPath = path.normalize(path.join(rootDir, filepath));
+        if (!normalizedPath.startsWith(rootDir)) {
+            await this.create4xx(403)
+            return;
+        }
+        try {
+            this._body = await readFile(normalizedPath, 'utf-8')
+            this._protocol = "HTTP/1.1"
+            this._status = 200
+            this._statusMsg = "OK"
+            this.addHeader("Content-Length:", this._body.length.toString())
+        }
+        catch (err) {
+            console.log("Error reading file:", err)
+            await this.create4xx(404)
+        }
+    }
+    parseResponse() {
+        let output = ""
+        output = this._protocol.concat(' ', this._status, ' ', this._statusMsg, '\r\n')
+        for(const [key, value] of this._headers) {
+            output = output.concat(key, ' ', value, "\r\n")
+        }
+        if(this._body) {
+            output = output.concat('\r\n', this._body)
+        }
+        return output
+    }
+}
+
+export {Request, Response}
---
+++ server/node_webserver/web_server.js
@@ -0,0 +1,36 @@
+"use strict";
+import { createServer } from "net";
+import {Request, Response} from "./helpers.js"
+function newConnection(socket) {
+    console.log('new connection incoming', socket.remoteAddress, socket.remotePort);
+    socket.on('end', function () {
+        console.log('EOF.');
+    });
+    socket.on('data', function (data) {
+        handleRequest(socket, data).catch(err => {
+            console.error("Error:", err);
+            socket.end("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+        });
+    });
+}
+async function handleRequest(socket, data) {
+    console.log('data:', data);
+    let req = new Request(data)
+    let res = new Response()
+    if(req.badRequest != 0) {
+        await res.create4xx(req.badRequest)
+    }
+    else {
+        await res.createResponse(req._resource)
+    }
+    console.log(res.parseResponse())
+    let x = Buffer.from(res.parseResponse(), 'utf8')
+    socket.write(x);
+    console.log("Answer sent");
+    socket.end();
+}
+
+
+var server = createServer();
+server.on('connection', newConnection);
+server.listen({ host: '127.0.0.1', port: 4242 });
